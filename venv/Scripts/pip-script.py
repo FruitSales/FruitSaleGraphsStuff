f68acb0129ae24f8eb7c6b41e6e523527c124e41
Dashel Bengs <beng512q01@ITACADEMY.LOCAL>
2020-01-27 10:39:16 -0500

    The first commit

---
+++ venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\beng512q01\Documents\python\python_word\Ch 17 Project\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
